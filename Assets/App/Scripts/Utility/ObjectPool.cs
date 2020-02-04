974db2e3b3bfba419f4588de83ff04512b194163
Kevin Pacheco <k.pacheco@me.com>
2020-02-04 12:46:21 -0800

    Update: Added Singleton, and Object Pool class.

---
+++ Assets/App/Scripts/Utility/ObjectPool.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+
+public class ObjectPool <T> : System.Object, System.IDisposable where T : class {
+    private List<T> pool = new List<T>();
+    private System.Func<T> create = null;
+    private System.Func<T, bool> isAvailable = null;
+    private System.Action<T> free = null;
+    private int capacity = 0;
+
+    public ObjectPool(System.Func<T> create, System.Func<T, bool> isAvailable, System.Action<T> free, int capacity = 0) {
+        this.create = create;
+        this.isAvailable = isAvailable;
+        this.free = free;
+
+        this.capacity = capacity;
+
+        if (this.capacity <= 0) this.capacity = int.MaxValue;
+
+        this.pool = new List<T>(this.capacity);
+	}
+
+    public virtual bool Get(out T o) {
+        for(int i = 0; i < this.pool.Count; ++i) {
+            var item = this.pool[i];
+
+            if(this.isAvailable(item)) {
+                o = item;
+
+                return true;
+			}
+        }
+
+        if(this.capacity < this.pool.Count) {
+            var clone = this.create();
+
+            o = clone;
+
+            return true;
+		}
+
+        o = default(T);
+        return false;
+	}
+
+    public void Dispose() {
+        for(int i = 0; i < this.pool.Count; ++i) {
+            if(this.pool[i] != null) {
+                this.free(this.pool[i]);
+			}
+		}
+
+        this.pool.Clear();
+        this.pool = null;
+        this.create = null;
+        this.isAvailable = null;
+        this.free = null;
+	}
+}
---
+++ Assets/App/Scripts/Utility/Singleton.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour where T : Singleton<T> {
+	private static T instance = null;
+	private static object mutex = new object();
+
+	public static T Instance {
+		get {
+			lock(mutex) {
+				if(instance == null) {
+					instance = Object.FindObjectOfType<T>();
+
+					if(instance == null) {
+						var go = new GameObject(typeof(T).Name);
+
+						go.SetActive(false);
+
+						instance = go.AddComponent<T>();
+
+						go.SetActive(true);
+					}
+
+					DontDestroyOnLoad(instance.gameObject);
+				}
+
+				return instance;
+			}
+		}
+	}
+
+	protected virtual void Awake() {
+		if(Instance != this) {
+			Destroy(this.gameObject);
+			return;
+		}
+	}
+}
