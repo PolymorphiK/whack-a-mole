a87070e1fbfccdc8730843252f6d3777a999617e
Kevin Pacheco <k.pacheco@me.com>
2020-02-11 15:18:54 -0800

    Update: SoDA update.

---
+++ Assets/App/Scripts/Actors/Mole.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(EventTrigger), typeof(Animator))]
+public class Mole : MonoBehaviour {
+    public void Whacked() {
+        Debug.Log("Got Whacked!");
+
+        Animator anim = GetComponent<Animator>();
+
+        anim.SetTrigger("Hide");  
+    }
+}
